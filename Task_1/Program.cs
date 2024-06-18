a26321133ad3a697ede2698e368a3662a7e43c99
kurilkoivan <101388849+kurilkoivan@users.noreply.github.com>
2024-06-18 18:22:34 +0500

    ====

---
+++ Task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int minutes = 30;
+            string dirPath = @"D:\Programming\Skillfactory\C#_projects\Module_8_FinalExercises\Module_8_FinalExercises\Task_1\FolderForTask1\TestFolder"; 
+            
+            DirectoryInfo directory = new DirectoryInfo(dirPath);
+            DeleteInFolder(directory,minutes);
+    
+        }
+
+        static void DeleteInFolder(DirectoryInfo directory,int minutes)
+        {
+            if (directory.Exists)
+            {
+                if (DateTime.Now - directory.LastAccessTime > TimeSpan.FromMinutes(minutes))
+                {
+                    foreach (FileInfo file in directory.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    Console.WriteLine("Все файлы удалены");
+                    foreach (DirectoryInfo dir in directory.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+                    Console.WriteLine("Все папки удалены");
+                }
+                else
+                {
+                    Console.WriteLine($"Время последнего доступа меньше {minutes} минут");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Папки не существует");
+            }
+        }
+    }
+}
